9659f8285ecb386ad92fff11f6fd530c109464c3
Wellington <wellington.namelless@gmail.com>
2017-10-23 00:21:51 -0300

    Alterando site

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function Enviar() {
+
+    var nome  = document.getElementById("nomeid");
+    var fone  = document.getElementById("foneid");
+    var text = document.getElementById("textarea");
+
+    if(nome.value == "" || nome.value.length > 80) {
+      alert( "Seu não pode ser vazio. Verifique!" );
+      nome.focus();
+      return false;
+    }
+    else if (email.value == "" || email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1 ){
+      alert( "Preencha campo e-mail corretamente!" );
+      email.focus();
+      return false;
+    }
+    else {
+      if(text.value == "" ) {
+        alert( "Este campo não pode ser vazio. Verifique!" );
+        text.focus();
+        return false;
+      }
+    }
+
+    if (nome.value != ""  && email.value != "" && text.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }else {
+        alert('Sr(a) ' + nome.value + ' alguns dados não foram preenchidos. Verifique!')
+    }
+
+}
